b08c3ab2e6c4c9d259ae86f39d33058046cf0dae
JangWonSeob <jqk9807@gmail.com>
2022-05-07 18:09:41 +0900

    Cors 작업

---
+++ src/main/java/com/website/trip/common/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.website.trip.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:8080")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name()
+                );
+    }
+}
